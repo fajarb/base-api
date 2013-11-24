5f10506207a2d8c95d0bb0e756722a41e152d486
Fajar Budiprasetyo <fajar.ab@gmail.com>
2013-11-24 14:00:29 +0700

    Add basic stuff

---
+++ Gemfile
@@ -0,0 +1,39 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.1'
+
+gem 'rails-api'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+gem 'mongoid', git: 'https://github.com/mongoid/mongoid.git'
+
+# Support for OAuth 2 provider
+gem 'doorkeeper'
+# Enumerated attributes
+gem 'enumerize'
+
+group :test, :development do
+  gem 'rspec-rails', '~> 3.0.0.beta'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  # gem 'fuubar'
+  # gem 'timecop'
+  gem 'simplecov'
+  # gem 'cane'
+  # gem 'morecane'
+end
+
+group :development do
+  gem 'capistrano'
+  gem 'jazz_hands'
+  gem 'better_errors'
+  # gem 'guard'
+  gem 'guard-rspec', require: false
+  gem 'growl'
+  gem 'rb-fsevent'
+  gem 'quiet_assets'
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+UniqDs::Application.config.secret_token = 'cf5d86acd30b6e68b5fb030a65745eb2481865abf49d0c3de3dc3253683ba8fc748997e8393ba74a4cc34b631d607af311ef76130abbac2b23e569930011ed0c'
